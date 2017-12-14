d87afe10915392c9a209e4b47cc2572ff4008310
Shawn Cicoria <github@cicoria.com>
2017-12-14 14:24:49 -0500

    fix so it works

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { UserAgentApplication, Logger } from 'msal';
+import { UserAgentApplication, Logger } from '@cicorias/msal';
 
 const applicationConfig = {
   clientID: process.env.REACT_APP_CLIENTID,
@@ -33,8 +33,6 @@ export default class Login extends Component {
       }
     );
 
-    var user = this.userAgentApplication.getUser();
-    this.warn(`the user is: ${JSON.stringify(user)}`)
   }
 
 
